82e1f0c6df3d98126f5c876e3562103fa4f00a3f
Razanhaweizy <Razanrh05@gmail.com>
2024-08-27 22:06:03 +0300

    add basic functionality

---
+++ game.js
@@ -0,0 +1,90 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let randomNum = Math.floor((Math.random() * 100));
+    if (randomNum === 0) {
+        return "rock";
+    }
+    else if (randomNum % 2 === 0) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Choose your pick: rock, paper, or scissors.");
+    if ((humanChoice === "rock") || (humanChoice === "paper") || (humanChoice === "scissors")) {
+        return humanChoice;
+    }
+    else {
+        prompt("Your input was invalid, try again");
+        return humanChoice;
+    }
+}
+
+function playRound(computerChoice, humanChoice) {
+    if (humanChoice === "rock") {
+        switch (computerChoice) {
+            case "rock": console.log("You have drawn!"); break;
+            case "paper": {
+                console.log("You lose :(");
+                computerScore++;
+                break;
+            }
+            case "scissors": {
+                console.log("You win!!! :)");
+                humanScore++;
+                break;
+            }
+        }
+    }
+    else if (humanChoice === "paper") {
+        switch(computerChoice) {
+            case "rock": {
+                console.log("You win! Yippie!!!");
+                humanScore++;
+                break;
+            }
+            case "paper": console.log("Draw ;/"); break;
+            case "scissors": {
+                console.log("Loser!!!");
+                computerScore++;
+                break;
+            }
+        }
+    }
+    else if (humanChoice === "scissors") {
+        switch(computerChoice) {
+            case "rock": {
+                console.log("Boohoo, you lose :(");
+                computerScore++;
+                break;
+            }
+            case "paper": {
+                console.log("Congrats! You win :)");
+                humanScore++;
+                break;
+            }
+            case "scissors": console.log("You tied ;/"); break;
+        }
+    }
+}
+
+/*
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound(getComputerChoice(), getHumanChoice());
+    }
+    if (computerScore > humanScore) {
+        console.log("You have lost the game :(");
+    }
+    else {
+        console.log("WINNERRRR!!! :)");
+    }
+}
+
+playGame()
+*/
